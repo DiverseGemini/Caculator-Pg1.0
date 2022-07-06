0b04d3caaeb15cfe548ba1f3ff99a6c8066b3128
Edward Spellman <trespellman@gmail.com>
2022-07-06 19:56:09 -0400

    Made progression

--- Window.cpp
+++ Window.cpp
@@ -4,16 +4,16 @@
 
 Window::Window() : wxFrame(nullptr, wxID_ANY, "Caculator", wxPoint(200, 200), wxSize(500, 600)) {
 
-	button = new wxButton(this, wxID_ANY, "1", wxPoint(10, 10), wxSize(100, 100));
-	button2 = new wxButton(this, wxID_ANY, "2", wxPoint(10, 10), wxSize(100, 100));
-	button3 = new wxButton(this, wxID_ANY, "3", wxPoint(10, 10), wxSize(100, 100));
-	button4 = new wxButton(this, wxID_ANY, "4", wxPoint(10, 10), wxSize(100, 100));
-	button5 = new wxButton(this, wxID_ANY, "5", wxPoint(10, 10), wxSize(100, 100));
-	button6 = new wxButton(this, wxID_ANY, "6", wxPoint(10, 10), wxSize(100, 100));
-	button7 = new wxButton(this, wxID_ANY, "7", wxPoint(10, 10), wxSize(100, 100));
-	button8 = new wxButton(this, wxID_ANY, "8", wxPoint(10, 10), wxSize(100, 100));
-	button9 = new wxButton(this, wxID_ANY, "9", wxPoint(10, 10), wxSize(100, 100));
-	buttonA = new wxButton(this, wxID_ANY, "dem  ", wxPoint(10, 10), wxSize(100, 100));
-	buttonB = new wxButton(this, wxID_ANY, "1", wxPoint(10, 10), wxSize(100, 100));
-	textBox = new wxTextCtrl(this, wxID_ANY,"",wxPoint(10,10),wxSize(600,100) ) ;
+	textBox = new wxTextCtrl(this, wxID_ANY,"",wxPoint(10,10),wxSize(473,100));
+
+	button = new wxButton(this, wxID_ANY, "1", wxPoint(10, 110), wxSize(90, 90));
+	button2 = new wxButton(this, wxID_ANY, "2", wxPoint(10, 210), wxSize(90, 90));
+	button3 = new wxButton(this, wxID_ANY, "3", wxPoint(10, 310), wxSize(90, 90));
+	button4 = new wxButton(this, wxID_ANY, "4", wxPoint(10, 410), wxSize(90, 90));
+	button5 = new wxButton(this, wxID_ANY, "5", wxPoint(100, 110),  wxSize(90, 90));
+	button7 = new wxButton(this, wxID_ANY, "7", wxPoint(100, 210), wxSize(90, 90));
+	button8 = new wxButton(this, wxID_ANY, "8", wxPoint(100, 310), wxSize(90, 90));
+	button9 = new wxButton(this, wxID_ANY, "9", wxPoint(100, 410), wxSize(90, 90));
+	buttonA = new wxButton(this, wxID_ANY, "dem  ", wxPoint(10, 10), wxSize(90, 90));
+	buttonB = new wxButton(this, wxID_ANY, "1", wxPoint(10, 10), wxSize(90, 90));
 }

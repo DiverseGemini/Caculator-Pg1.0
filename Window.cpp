3239e68b8bbc83e4427885c320a4a5f6bc7c06ee
Edward Spellman <trespellman@gmail.com>
2022-07-08 18:18:15 -0400

    Fixed error 1.1

--- Window.cpp
+++ Window.cpp
@@ -7,6 +7,7 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Caculator", wxPoint(0, 200), wxSi
 	textBox = new wxTextCtrl(this, wxID_ANY, "", wxPoint(0, 10), wxSize(270, 100));
 
 	//Frist row of numbers
+	button0 = new wxButton(this, wxID_ANY, "0", wxPoint(182, 110), wxSize(90, 90));
 	button = new wxButton(this, wxID_ANY, "1", wxPoint(2, 200), wxSize(90, 90));
 	button2 = new wxButton(this, wxID_ANY, "2", wxPoint(92, 200), wxSize(90, 90));
 	button3 = new wxButton(this, wxID_ANY, "3", wxPoint(182, 200), wxSize(90, 90));
@@ -24,6 +25,7 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Caculator", wxPoint(0, 200), wxSi
 	//PemDas
 	buttonA = new wxButton(this, wxID_ANY, "+", wxPoint(2, 110), wxSize(45, 30));
 	buttonB = new wxButton(this, wxID_ANY, "-", wxPoint(47, 110), wxSize(45, 30));
+	button2B = new wxButton(this, wxID_ANY, "(-)", wxPoint(2, 170), wxSize(45, 30));
 	buttonC = new wxButton(this, wxID_ANY, "*", wxPoint(92, 110), wxSize(45, 30));
 	buttonD = new wxButton(this, wxID_ANY, "/", wxPoint(137, 110), wxSize(45, 30));
 
@@ -33,7 +35,7 @@ Window::Window() : wxFrame(nullptr, wxID_ANY, "Caculator", wxPoint(0, 200), wxSi
 	buttonJ = new wxButton(this, wxID_ANY, "Mod", wxPoint(137, 140), wxSize(45, 30));
 
 	//clear and anwser button
-	buttonE = new wxButton(this, wxID_ANY, "=", wxPoint(183, 110), wxSize(45, 30));
-	buttonF = new wxButton(this, wxID_ANY, "Clear", wxPoint(183, 140), wxSize(45, 30));
+	buttonE = new wxButton(this, wxID_ANY, "=", wxPoint(47, 170), wxSize(45, 30));
+	buttonF = new wxButton(this, wxID_ANY, "Clear", wxPoint(92, 170), wxSize(45, 30));
 
 }
